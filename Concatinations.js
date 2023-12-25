983da67121699182d0dcf875e28200b3a381f011
Sruthi.Pasumarthi <pvsruthi22@gmail.com>
2023-12-25 22:51:01 +0530

    Data Types assignment

---
+++ Concatinations.js
@@ -0,0 +1,10 @@
+a = 10
+b = "hai"
+c = true
+
+console.log(a+b+c)
+console.log(a+b)
+console.log(a+c)
+console.log(b+c)
+console.log(b+c+a)
+console.log(c+a+b)
---
+++ Conversions.js
@@ -0,0 +1,25 @@
+a = {}
+console.log(typeof a)
+console.log(Number(a))
+console.log(String (a))
+console.log(Boolean (a))
+b = NaN
+console.log(typeof b)
+console.log(Number(b))
+console.log(String (b))
+console.log(Boolean (b))
+c = -Infinity
+console.log(typeof c)
+console.log(Number(c))
+console.log(String (c))
+console.log(Boolean (c))
+d = function(){}
+console.log(typeof d)
+console.log(Number(d))
+console.log(String (d))
+console.log(Boolean (d))
+e = ["twenty"]
+console.log(typeof e)
+console.log(Number(e))
+console.log(String (e))
+console.log(Boolean (e))
